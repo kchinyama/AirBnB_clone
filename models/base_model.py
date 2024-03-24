0b5a7172df25318fa45187455bb378275a8d0eef
root <kaumba@kalichipictures.com>
2024-03-24 06:25:33 -0700

    task 3

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""BaseModel module that will define all the common attributes
+and methods for all classes"""
+
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel():
+    """BaseModel class that contains all methods and attributes
+    of our instances"""
+
+    def __init__(self):
+        """our constructor for each instance that will be created"""
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """human readable rep of all my instances"""
+
+        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+
+    def save(self):
+        """updates time stamp on updated_at time stamp"""
+
+        updated_at = datetime.now
+
+    def to_dict(self):
+        """returns key, value pairs of the __dict__ of instance"""
+
+        pyDictObjs = {}
+
+        pyDictObjs['__class__'] = self.__class__.__name__
+
+        for key, value in self.__dict__.items():
+
+            if isinstance(value, datetime):
+                pyDictObjs[key] = datetime.now().isoformat()
+
+            else:
+                pyDictObjs[key] = value
+
+        return pyDictObjs

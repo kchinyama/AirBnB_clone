ed417f9359e4d195abd6ef97b626530c361fa5a8
root <kaumba@kalichipictures.com>
2024-03-24 07:18:12 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -14,12 +14,25 @@ class BaseModel():
     """BaseModel class that contains all methods and attributes
     of our instances"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """our constructor for each instance that will be created"""
 
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            del kwargs['__class__']
+
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    dTimeObj = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                            )
+                    setattr(self, key, dTimeObj)
+
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+           # self.updated_at = datetime.now()
 
     def __str__(self):
         """human readable rep of all my instances"""
